983997154dfdfb94a0d25e8f15dc12408987ebbc
Andrew Dickerson <andrew.j.dickerson@intel.com>
2017-08-02 14:54:38 -0700

    Scatter graphs for report results

--- src/app/results/results/resultview/resultview.component.ts
+++ src/app/results/results/resultview/resultview.component.ts
@@ -11,38 +11,101 @@ const colors = window.require('nice-color-palettes');
 
 export class ResultViewComponent implements OnChanges {
   data: any;
+  options: any;
   @Input() result: any;
 
   constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnChanges() {
-    this.getData();
+    // this.getData();
 
-    const colorSet = colors[0].concat(colors[1]);
 
-    this.data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          borderColor: colorSet[0]
-        },
-        {
-          label: 'Second Dataset',
-          data: [28, 48, 40, 19, 86, 27, 90],
-          fill: false,
-          borderColor: colorSet[1]
-        }
-      ]
-    };
+
+    // Datasets
+    /*
+      Create an array with length equal to
+     */
+
+
+    // this.data = {
+    //   datasets: [{
+    //     label: 'Scatter Dataset',
+    //     data: [{
+    //       x: -10,
+    //       y: 0
+    //     }, {
+    //       x: 0,
+    //       y: 10
+    //     }, {
+    //       x: 10,
+    //       y: 5
+    //     }]
+    //   }]
+    // };
+
+    this.data = this.getData();
+
+    // this.options = {
+    //   scales: {
+    //     xAxes: [
+    //       {display: false}
+    //     ]
+    //   }
+    // };
+  }
+
+  asTime(runTime) {
+    return moment(runTime).format('HH:MM:SS MM/DD/YYYY');
+  }
+
+  fromTime(runTime) {
+    return moment(runTime).fromNow();
+  }
+
+  ignoreResult(run) {
+    console.log('Ignoring result');
   }
 
   getData() {
     if (!this.result || !this.result.results.length) {
       return;
     }
+
+    const colorSet = colors[0].concat(colors[1]);
+
+    const environments = this.result.results.map((item) => {
+      return item.host;
+    }).filter((item, index, arr) => {
+      return arr.indexOf(item) === index;
+    });
+
+    const groupedByHost = Array.from({length: environments.length}, i => []);
+
+    this.result.results.forEach((item) => {
+      const index = environments.indexOf(item.host);
+
+      groupedByHost[index].push(item);
+    });
+
+    const asScatterPoints = [];
+
+    groupedByHost.forEach((item, index) => {
+      asScatterPoints.push({
+        label: item[0].host,
+        backgroundColor: colorSet[index],
+        borderColor: colorSet[index],
+        data: item.map((res) => {
+          return {
+            x: moment(res.endTime).format('DDDDHHMM'),
+            y: parseFloat(res.elapsed)
+          };
+        })
+      });
+    });
+
+    return {
+      datasets: asScatterPoints
+    };
 
     // const times = this.result.results.map((item) => {
     //   return item.endTime;
